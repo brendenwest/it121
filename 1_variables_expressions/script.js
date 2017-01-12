660b0acc6473fa7b42cb210c65e5c5c62f76d7e9
Erin R Martin <erinrmartin@yahoo.com>
2017-01-12 01:04:38 -0800

    Add files via upload

---
+++ 1_variables_expressions/script.js
@@ -0,0 +1,15 @@
+//this is a script file
+
+
+var firstName = "Erin"
+document.write(firstName);
+
+var lastName = "Martin";
+document.write(lastName);
+
+var yearsOfStudy = "2nd quarters";
+document.write(yearsOfStudy);
+
+var goal = "get a better paying job that I actually like";
+document.write(goal);
+
